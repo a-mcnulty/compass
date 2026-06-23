9a0518111aa27e61fdd9b6dfa40bf291a996fb0c
Alexander McNulty <amcnulty16180@gmail.com>
2026-06-23 11:35:49 -0700

    new attempt to program compass

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
+heading = 0
+
 def on_forever():
-    if (337.5) >= (22.5):
+    global heading
+    heading = input.compass_heading()
+    if heading < 23 or heading >= 338:
         basic.show_arrow(ArrowNames.NORTH)
-basic.forever(on_forever)
-
-def on_forever2():
-    if (22.5) <= (67.5):
+    elif heading < 68:
         basic.show_arrow(ArrowNames.NORTH_EAST)
-basic.forever(on_forever2)
-
-def on_forever3():
-    if (67.5) <= (112.5):
+    elif heading < 113:
         basic.show_arrow(ArrowNames.EAST)
-basic.forever(on_forever3)
-
-def on_forever4():
-    if (112.5) <= (157.5):
+    elif heading < 158:
         basic.show_arrow(ArrowNames.SOUTH_EAST)
-basic.forever(on_forever4)
-
-def on_forever5():
-    if (157.5) <= (202.5):
+    elif heading < 203:
         basic.show_arrow(ArrowNames.SOUTH)
-basic.forever(on_forever5)
-
-def on_forever6():
-    if (202.5) <= (247.5):
+    elif heading < 248:
         basic.show_arrow(ArrowNames.SOUTH_WEST)
-basic.forever(on_forever6)
-
-def on_forever7():
-    if (247.5) <= (292.5):
+    elif heading < 293:
         basic.show_arrow(ArrowNames.WEST)
-basic.forever(on_forever7)
-
-def on_forever8():
-    if (292.5) <= (337.5):
+    else:
         basic.show_arrow(ArrowNames.NORTH_WEST)
-basic.forever(on_forever8)
+basic.forever(on_forever)
